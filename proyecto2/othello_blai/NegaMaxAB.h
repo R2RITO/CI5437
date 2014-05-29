07a57f3c7c33d0620e827409ac1213a53d20128e
andresosw jiemenz <andres_osw@hotmail.com>
2014-05-29 15:38:12 -0430

    NegamaxAB

--- proyecto2/othello_blai/NegaMaxAB.h
+++ proyecto2/othello_blai/NegaMaxAB.h
@@ -13,31 +13,25 @@ public:
         }
           
 	    int m = alpha;
-        int res_negamax_ab;
+        int res_negamax_ab = 0;
         state_t children;
         bool tmp = false;
 
         for( int pos = 0; pos < DIM; ++pos ) {
         	
-            
         	if((color && state.is_black_move(pos))||(!color && state.is_white_move(pos))) {
                 children = state.move(color,pos);
                 res_negamax_ab = (-1) * NegaMaxAB(children,depth-1,!color,(-1)*beta,(-1)*m);
-                if (res_negamax_ab > m ) {
-                   m = res_negamax_ab;
-                   tmp = true;
-                }
-                if (m >= beta) { 
-                   return m; 
-                }
+                if (res_negamax_ab > m ) m = res_negamax_ab;
+                if (m >= beta) return m; 
+                tmp = true;
             }                      
         }
         
         if (!tmp) {
-            res_negamax_ab = (-1) * NegaMaxAB(state,depth-1,!color,(-1)*beta,(-1)*m);
+            m = (-1) * NegaMaxAB(state, depth-1, !color,(-1)*beta,(-1)*m);
         }
 
         return m;
     }
-
 };
